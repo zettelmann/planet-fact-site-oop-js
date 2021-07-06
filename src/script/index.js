57a0b59f72b4f7a552b768461d83563e8a3263df
Sebastian Zettelmann <sebastian.zettelmann@gmx.de>
2021-07-06 21:49:09 +0200

    finished layout

--- src/script/index.js
+++ src/script/index.js
@@ -5,3 +5,6 @@ import './mobileMenu';
 
 
 
+
+
+
--- src/script/mobileMenu.js
+++ src/script/mobileMenu.js
@@ -1,11 +1,27 @@
+/* 
+    MOBILE NAVIGATION TOGGLE
+*/
 const mobileMenuBtn = document.querySelector('.mobile-menu__button');
 const mobileMenuIcon = document.querySelector('.mobile-menu__icon');
 const mobileMenuNav = document.querySelector('.nav');
 
 
 const showMobileNavigation = () => {
-    mobileMenuBtn.addEventListener('click', ()=> {
+    mobileMenuBtn.addEventListener('click', () => {
         mobileMenuNav.classList.toggle('show-mobile-menu');
     })
 }
 showMobileNavigation();
+/* 
+    MOBILE SUB-NAVIGATION 
+*/
+const mobileSubNavContainer = document.querySelector('.planet-facts__btns-list')
+const mobileSubNavBtn = document.querySelectorAll('.planet-facts__btn');
+
+mobileSubNavContainer.addEventListener('click', e => {
+    const clicked = e.target.closest('.planet-facts__btn');
+    if(!clicked) return;
+    
+    mobileSubNavBtn.forEach(btn => btn.classList.remove('show-planet-facts'));
+    clicked.classList.add('show-planet-facts');
+});
