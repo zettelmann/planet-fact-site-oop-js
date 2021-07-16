19c9970fabb31fd8780532c7f50e943adadbde2d
Sebastian Zettelmann <sebastian.zettelmann@gmx.de>
2021-07-16 14:43:51 +0200

    fixed webpack output

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   target: "web",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname),
     clean: true,
   },
   devtool: "inline-source-map",
